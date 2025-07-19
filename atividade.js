22e4fed540c032b8157c3b0319a53522dee02c6b
Samuel <baltaz.samuel@gmail.com>
2025-07-19 03:04:59 -0300

    feature: implementando template da atividade com refacts

--- atividade.js
+++ atividade.js
@@ -1,6 +1,7 @@
 /* 
 EJCM TT 25.1 - Exercicio JS
-Seu trabalho e criar um pequeno programa que deve processar as notas dos alunos, calcular a média da turma e determinar a situação de cada aluno. Para isso preencha o conteudo das funcoes: processarNotas(), calcularMediaNotas(), mostrarSituacaoAluno()
+Seu trabalho e criar um pequeno programa que deve processar as notas dos alunos, calcular a média da turma e determinar a situação de 
+cada aluno. Para isso preencha o conteudo das funcoes: processarNotas(), calcularMediaNotas(), mostrarSituacaoAluno()
 
 Abaixo ha um template e instrucoes das funcoes para que voces preencham
 
@@ -11,12 +12,16 @@ A funcao processarNotas() ira receber uma String notasString e mostrar um array 
 
 A funcao calcularMediaNotas() ira receber uma String notasString e ira retornar um numero mediaTurma
 
-A funcao mostrarSituacaoAluno() ira receber uma String notasString e ira mostrar no terminal um Object situacaoAluno onde a Key devera ser a nota do aluno e o Value devera ser uma String "Aprovado e Acima da media da turma", "Reprovado e Acima da media da turma", "Aprovado e Abaixo da media da turma" ou "Reprovado e Abaixo da media da turma". Alunos com nota igual ou maior que 5.0 estarao Aprovados
+A funcao mostrarSituacaoAluno() ira receber uma String notasString e ira mostrar no terminal um Object situacaoAluno onde a Key devera 
+ser a nota do aluno e o Value devera ser uma String "Aprovado e Acima da media da turma", "Reprovado e Acima da media da turma", 
+"Aprovado e Abaixo da media da turma" ou "Reprovado e Abaixo da media da turma". Alunos com nota igual ou maior que 5.0 estarao 
+Aprovados
 
 
 
 Exemplo: para let notasString = "7.5, 8.0, 6.2, 9.5, 4.0";
-o console deve mostrar { '7.5': 'Aprovado e Acima da media da turma', '8.0': 'Aprovado e Acima da media da turma', '6.2': 'Aprovado e Abaixo da media da turma', '9.5': 'Aprovado e Acima da media da turma', '4.0': 'Reprovado e Abaixo da media da turma' }
+o console deve mostrar { '7.5': 'Aprovado e Acima da media da turma', '8.0': 'Aprovado e Acima da media da turma', '6.2': 'Aprovado e 
+Abaixo da media da turma', '9.5': 'Aprovado e Acima da media da turma', '4.0': 'Reprovado e Abaixo da media da turma' }
 */
 
 
@@ -37,5 +42,5 @@ function mostrarSituacaoAluno(notasString){
 }
 
 
-//Ao terminar remova o comentario da linha abaixo para que a funcao principal seja chamada e utilize node atividade.js no terminal para rodar o codigo
+//Ao terminar remova o comentario da linha abaixo para que a funcao principal seja chamada e utilize node atividade.js no terminal
 //mostrarSituacaoAluno()
