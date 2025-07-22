813a1c89cdbc9e5ffa6fb035381c640c53ef9143
Samueldnz <samuelsd@dcc.ufrj.br>
2025-07-21 21:31:40 -0300

    init: contribuição de Samuel Sampaio à atividade 1 de JS do TT/EJCM

--- atividade.js
+++ atividade.js
@@ -26,21 +26,47 @@ Abaixo da media da turma', '9.5': 'Aprovado e Acima da media da turma', '4.0': '
 
 
 
-function processarNotas(notasString){
-
-    return notasNumericas
+function processarNotas(notasString) {
+    // Converte a string para um array de números
+    let notasNumericas = notasString.split(',').map(nota => parseFloat(nota.trim()));
+    return notasNumericas;
 }
 
-function calcularMediaNotas(notasString){
-
-    return mediaTurma
+function calcularMediaNotas(notasString) {
+    let notas = processarNotas(notasString);
+    let soma = notas.reduce((acumulador, nota) => acumulador + nota, 0);
+    let mediaTurma = soma / notas.length;
+    return mediaTurma;
 }
 
-function mostrarSituacaoAluno(notasString){
+function mostrarSituacaoAluno(notasString) {
+    let notas = processarNotas(notasString);
+    let mediaTurma = calcularMediaNotas(notasString);
+    let situacaoAluno = {};
 
-    console.log(situacaoAluno)
+    notas.forEach(nota => {
+        let aprovado = nota >= 5.0;
+        let acimaMedia = nota >= mediaTurma;
+
+        if (aprovado && acimaMedia) {
+            situacaoAluno[nota] = "Aprovado e Acima da media da turma";
+        } else if (!aprovado && acimaMedia) {
+            situacaoAluno[nota] = "Reprovado e Acima da media da turma";
+        } else if (aprovado && !acimaMedia) {
+            situacaoAluno[nota] = "Aprovado e Abaixo da media da turma";
+        } else {
+            situacaoAluno[nota] = "Reprovado e Abaixo da media da turma";
+        }
+    });
+
+    console.log(situacaoAluno);
 }
+
+// Exemplo de uso:
+let notasString = "7.5, 8.0, 6.2, 9.5, 4.0";
+
+
 
 
 //Ao terminar remova o comentario da linha abaixo para que a funcao principal seja chamada e utilize node atividade.js no terminal
-//mostrarSituacaoAluno()
+mostrarSituacaoAluno(notasString);
